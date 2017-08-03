4bcce05af2491e2958e1b1b3dc37caf79e9d31e5
wangtao <wangtao@weibopay.com>
2017-08-03 14:10:08 +0800

    remove integrationTest

--- test.webpack.js
+++ test.webpack.js
@@ -1,4 +1,4 @@
 const context = require.context("./src", true, /\.spec\.jsx?$/);
 context.keys().forEach(context);
-const integrationContext = require.context("./test", true, /\.jsx?$/);
-integrationContext.keys().forEach(integrationContext);
+// const integrationContext = require.context("./test", true, /\.jsx?$/);
+// integrationContext.keys().forEach(integrationContext);
