1783deaafbe2ad734d2c178ae52cb379a7775e7a
hapood <wanglejia@gmail.com>
2017-08-19 13:17:50 +0800

    fix test

--- src/SceneSwitch/SceneSwitch.spec.js
+++ src/SceneSwitch/SceneSwitch.spec.js
@@ -6,12 +6,17 @@ import sinon from "sinon";
 import SceneSwitch from "./SceneSwitch";
 
 describe("<SceneSwitch />", () => {
-  // it("set history when ReduxArena is mounted", () => {
-  //   const setArenaHistorySpy = sinon.spy();
-  //   const wrapper = shallow(
-  //     <ReduxArena setArenaHistory={setArenaHistorySpy} />
-  //   );
-  //   expect(setArenaHistorySpy).to.have.property("callCount", 1);
-  //   assert.notEqual(setArenaHistorySpy.args[0].push, null);
-  // });
+  it("should add reducer and render Switch", () => {
+    const addReducerSpy = sinon.spy();
+    const dispatchSpy = sinon.spy();
+    const ctx = {
+      store: {
+        addReducer: addReducerSpy,
+        dispatch: dispatchSpy
+      }
+    };
+    const wrapper = shallow(<SceneSwitch />, { context: ctx });
+    expect(addReducerSpy).to.have.property("callCount", 1);
+    expect(dispatchSpy).to.have.property("callCount", 1);
+  });
 });
