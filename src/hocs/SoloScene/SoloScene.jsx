13a42a9d8dd67b77b3a6f6fd10e651f9a0883627
wangtao <wangtao@weibopay.com>
2017-09-18 10:56:28 +0800

    fix isNotifyOn pass bug

--- src/hocs/SoloScene/SoloScene.jsx
+++ src/hocs/SoloScene/SoloScene.jsx
@@ -37,6 +37,7 @@ export default class SoloScene extends Component {
       asyncSceneBundle,
       sceneBundle,
       sceneProps,
+      isNotifyOn,
       notifyData,
       SceneLoadingComponent
     } = this.props;
@@ -50,6 +51,7 @@ export default class SoloScene extends Component {
       asyncSceneBundle,
       sceneBundle,
       sceneProps,
+      isNotifyOn,
       notifyData,
       SceneLoadingComponent
     });
@@ -75,6 +77,7 @@ export default class SoloScene extends Component {
       asyncSceneBundle,
       sceneBundle,
       sceneProps,
+      isNotifyOn,
       notifyData,
       SceneLoadingComponent
     } = nextProps;
@@ -131,6 +134,7 @@ export default class SoloScene extends Component {
           asyncSceneBundle,
           sceneBundle,
           sceneProps,
+          isNotifyOn,
           notifyData,
           SceneLoadingComponent
         })
